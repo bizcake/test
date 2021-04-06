206516ec9835b17744d23afd7363e777574ee01b
DESKTOP-7D86IBB\admin <batteryz@naver.com>
2021-04-06 11:14:18 +0900

    aa

--- sample.js
+++ sample.js
@@ -3,24 +3,15 @@ function add_master2 (x, y){
 	console.log(y);
 	console.log(x + y);
 	console.log(0)
-	
-	
-	
-	
-	
+
 	return x + y;
 }
+
 
 function add_test22 (x, y){
 	console.log(x);
 	console.log(y);
 	console.log(x + y);
-	console.log(0)
-	
-	
-	
-	
-	
 	return x + y;
 }
 
